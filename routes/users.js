2aac2ba764578b569d7ded8b3a8dc09a21f99099
aashish <aashish.kumar@mobcoder.com>
2025-10-28 18:06:11 +0530

    synyax issue for signin route corrected , next issue is loged in user not coming in blog , but rik is coming

--- routes/users.js
+++ routes/users.js
@@ -30,16 +30,20 @@ router.post("/signup", async (req, res)=>{
 })
 
 router.post("/signin", async (req, res) => {
+    console.log(req.body , "line 33 in users.js")
     const { email, password } = req.body;
 
     // 1️⃣ Find the user by email
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ where : {email} });
     if (!user) {
         return res.status(400).send("User not found");
     }
 
+    console.log(user , password, "file is users.js line 41")
+
     // 2️⃣ Compare the password with bcrypt
     const isMatch = await bcrypt.compare(password, user.password);
+    console.log(isMatch , "fike is users.js") 
     if (!isMatch) {
         return res.status(400).render("signin", {
             error : "Incorrect password "
