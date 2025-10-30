6a367327eb4fa59261ce17fdc29a97d08a0d4cdb
aashikofsach <ritikkamboj4314@gmail.com>
2025-10-30 22:31:35 +0530

    making changes to make deployement ready

--- config/db.js
+++ config/db.js
@@ -1,9 +1,26 @@
-import { Sequelize } from 'sequelize';
+// import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('youtube_blog', 'postgres', 'Ladwa@123', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false,
-});
+// const sequelize = new Sequelize('youtube_blog', 'postgres', 'Ladwa@123', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   logging: false,
+// });
+
+// export default sequelize;
+
+
+
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 export default sequelize;
